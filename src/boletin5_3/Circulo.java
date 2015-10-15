88fb341594e44fefb7978c88a5b214e23041d8b0
joterodelrio <joterodelrio@atanasoff16.danielcastelao.org>
2015-10-15 12:40:55 +0200

    Commit inicial

---
+++ src/boletin5_3/Circulo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_3;
+
+/**
+ *
+ * @author joterodelrio
+ */
+public class Circulo {
+    private double radio;
+    final double PI = 3.14;
+    //Constructores
+    public Circulo(){}
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+    public void setRadio(double radio){
+        this.radio = radio;
+    }
+    public double getRadio(){
+        return radio;
+    }
+    public double calcularArea(){
+        return PI * Math.pow(radio,2);
+    }
+    public double calcularLongitud(){
+        return 2 * PI * radio;
+    }
+}
